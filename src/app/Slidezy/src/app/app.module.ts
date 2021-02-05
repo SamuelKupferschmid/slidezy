9875086255fd2515d4ea857ce1b94049faa8d2de
Samuel Kupferschmid <samuel.kupferschmid@gmail.com>
2021-02-05 23:05:03 +0100

    add angular material

--- src/app/Slidezy/src/app/app.module.ts
+++ src/app/Slidezy/src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
