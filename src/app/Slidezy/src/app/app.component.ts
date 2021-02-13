f1e9482eef82480128224e44c7192aec62e292f8
Samuel Kupferschmid <samuel.kupferschmid@gmail.com>
2021-02-13 21:50:11 +0100

    fullscreen mode

--- src/app/Slidezy/src/app/app.component.ts
+++ src/app/Slidezy/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Session, SlidesService } from './slide/slides.service';
@@ -13,9 +13,19 @@ export class AppComponent {
 
   session$: Observable<{ session: Session }>;
 
+  @ViewChild('app') appRef: ElementRef<HTMLDivElement>;
+
   constructor(slideService: SlidesService) {
     this.session$ = slideService.session$.pipe(
       map(session => ({ session }))
     );
   }
+
+  enterFullscreen(mode: FullscreenMode) {
+    const element = mode === 'app' ? this.appRef.nativeElement : document.querySelector('#canvas');
+    element.requestFullscreen();
+
+  }
 }
+
+type FullscreenMode = 'app' | 'canvas';
--- src/app/Slidezy/src/app/app.module.ts
+++ src/app/Slidezy/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 import { HomeComponent } from './home/home.component';
 import { SlideListComponent } from './slide-list/slide-list.component';
 import { NewSessionComponent } from './new-session/new-session.component';
@@ -42,6 +43,7 @@ const routes: Routes = [
     MatButtonModule,
     MatSidenavModule,
     MatCardModule,
+    MatMenuModule,
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
